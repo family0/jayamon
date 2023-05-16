383adb8e1e7c20c7419962e5ce7a033781928d2d
TJ <tj@itst.co.kr>
2023-05-16 22:53:11 +0900

    Spring init

---
+++ src/test/kotlin/org/itstory/jayamon/JayamonApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.itstory.jayamon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JayamonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
